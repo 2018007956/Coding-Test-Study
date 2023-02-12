d15b08b740e8ee4461e4481c0dc2866ddf3f30b4
2018007956 <bbmt1906@hanyang.ac.kr>
2023-02-12 22:56:20 +0900

    230212

--- week6/10026_CA.py
+++ week6/10026_CA.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 # up, down, left, right
 row_dr = [0,0,-1,1]
 col_dr = [-1,1,0,0]
@@ -12,11 +15,12 @@ def dfs(x,y):
             continue
         
         if not(visited[nx][ny]):
-            visited[nx][ny] = True
-            if pic[x][y]==pic[nx][ny]:
+            if pic[x][y]==pic[nx][ny]:            
+                visited[nx][ny] = True
                 dfs(nx,ny)
-            else:
-                guyuk += 1
+            # else:
+            #     guyuk += 1
+            
 
 N = int(input())
 pic = []
@@ -25,13 +29,23 @@ for _ in range(N):
 
 visited = [[False]*len(pic[0]) for _ in range((N))]
 guyuk = 0
-dfs(0,0)
+for i in range(N):
+    for j in range(len(pic[0])):
+        if not visited[i][j]:
+            dfs(i,j)
+            guyuk += 1
 print(guyuk, end=' ')
 
-# 적록색약인경우
+# 적록색약인 경우
 visited = [[False]*len(pic[0]) for _ in range((N))]
 for i in range(N):
     pic[i] = pic[i].replace('R','G')
+    pic[i] = pic[i].replace('r','g')
+    
 guyuk = 0
-dfs(0,0)
+for i in range(N):
+    for j in range(len(pic[0])):
+        if not visited[i][j]:
+            dfs(i,j)
+            guyuk += 1
 print(guyuk)
