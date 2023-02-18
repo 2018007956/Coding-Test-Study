4a9fded186d8b3ab8f6ae69a0b5f112046cb447b
KyeongHa <rudgk030@hanyang.ac.kr>
2023-02-18 20:03:57 +0900

    week7_KH

--- week7/2110_KH.js
+++ week7/2110_KH.js
@@ -1,121 +1,29 @@
 // 공유기 설치
-const input = require('fs').readFileSync('test.txt').toString().trim().split(/\s/).map(Number);
-let [numberOfHouse, numberOfRouter, ...house] = input;
-let router = [];
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let [numberOfHouse, numberOfRouter] = input[0].split(' ').map(Number);
+let [...house] = input.slice(1).map(Number).sort((a,b) => a-b);
 
+// house는 1부터 numberOfHouse 공간에 위치할 수 있음
+let start = 1;
+let end = house[house.length - 1] - house[0];
+let result = 0;
 
+while (start <= end) {
+  let mid = Math.floor((start + end) / 2);
+  let value = house[0];
+  let installedRouter = 1;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function getClosetIndex(a, b){
-//     let sum = 0;
-//     for(let i=a; i<=b; i++) sum += house[i];
-
-//     const mid = (sum / (b-a+1));
-
-//     console.log(mid);
-//     let min = house[b];
-//     let closetIndex = 0;
-
-//     for(let i=a+1; i<=b; i++){
-//         const abs = Math.abs(house[i] - mid);
-//         console.log(abs);
-//         if(abs <= min){
-//             min = abs;
-//             closetIndex = i
-//         }
-//     }
-//     return closetIndex;
-// }
-
-// // function getMaxDistance(){
-// //     let maxDistance = 0;
-// //     let idxA = 0;
-// //     let idxB = 0;
-
-// //     for(let i=0; i<house.length-1; i++){
-// //         const distance = house[i+1] - house[i];
-
-// //         if(distance > maxDistance){
-// //             maxDistance = distance;
-// //             idxA = i; idxB = i+1;
-// //         }
-// //     }
-
-// //     return [idxA, idxB];
-// // }
-
-// function setRouter(start, end) {
-
-//     // while(true){
-//     //     const targetIndex = getMaxDistance();
-//     //     console.log(targetIndex);
-        
-//     //     router.push(targetIndex[0]);
-//     //     house.splice(targetIndex[0], 1);
-//     //     numberOfRouter--;
-
-//     //     if(numberOfRouter < 0) return;
-
-//     //     router.push(targetIndex[1]);
-//     //     house.splice(targetIndex[1], 1);
-//     //     numberOfRouter--;
-
-//     //     if(numberOfRouter < 0) return;
-//     // }
-//     // if(numberOfRouter == 1){
-//     //     router.push(house[end]);
-//     //     numberOfRouter--;
-//     //     return;
-//     // }
-//     if(start+1 >= end-1 || numberOfRouter <= 0) return;
-
-//     console.log(start + ' ' + end)
-//     const mid = getClosetIndex(start, end);
-
-//     router.push(house[mid]);
-//     numberOfRouter--;
-
-//     setRouter(start, mid);
-//     setRouter(mid, end);
-// }
-
-// function getMinDistance(){
-//     let minDistance = Infinity;
-
-//     for(let i=0; i<router.length-1; i++){
-//         const distance = router[i+1] - router[i];
-
-//         if(distance < minDistance) 
-//             minDistance = distance;
-//     }
-
-//     return minDistance;
-// }
-
-// house.sort((a, b) => a - b);
-
-// router.push(house[0]);
-// router.push(house[numberOfHouse-1]);
-// numberOfRouter -= 2;
-
-// setRouter(0, numberOfHouse-1);
-// if(numberOfRouter >= 1) router.push(house[numberOfHouse-1]);
-
-
-// router.sort((a, b) => a - b);
-// console.log(router)
-// console.log(getMinDistance());
+  for (let i=1; i <numberOfHouse; i++) {
+    if (house[i] >= value + mid) {
+      value = house[i];
+      installedRouter++;
+    }
+  }
+  if (installedRouter >= numberOfRouter) {
+    start = mid + 1;
+    result = mid;
+  } else {
+    end = mid - 1;
+  }
+}
+console.log(result);
