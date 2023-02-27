190bb6b62b6ba57f2de0547700d71c50a7f680b9
2018007956 <bbmt1906@hanyang.ac.kr>
2023-02-27 14:10:05 +0900

    230227

--- week8/1783_CA.py
+++ week8/1783_CA.py
@@ -7,7 +7,7 @@ x, y = 0,0
 visited = [[False]*M for _ in range(N)] 
 visited[0][0] = True
 if N>M:
-        m = (move[1],move[2])
+    m = (move[1],move[2])
 else:
     m = (move[0],move[3])
 
@@ -24,6 +24,8 @@ while prev_cnt!=cnt:
 print(cnt)
 
 '''
+시도중...
+
 [병든 나이트의 이동 횟수가 4번보다 적지 않다면, 
 이동 방법을 모두 한 번씩 사용해야 한다]
 이 조건을 어떻게 적용시켜야 할까?
