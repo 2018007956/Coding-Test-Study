d4f849efe3066d741a60c7e1955d9a67835f57e5
2018007956 <bbmt1906@hanyang.ac.kr>
2023-03-05 23:34:16 +0900

    230305

--- week9/15683_CA.py
+++ week9/15683_CA.py
@@ -1,15 +1,36 @@
+from collections import deque
+def dfs(x):
+    for direction in range(len(cctv[arr[x][y]])):
+        # arr reset
+        chk = [x[:] for x in arr]
+        for k in direction:
+            nx = x+row_dr[k]
+            ny = y+col_dr[k]
+            if nx<0 or ny<0 or nx>=M or ny>=N or chk[nx][ny]!=0:
+                continue
+            dfs(y)
+
 N, M = map(int, input().split())
 arr = []
 for _ in range(N):
     arr.append(input().split())
 
-one = [['up'],['down'],['left'],['right']]
-two = [['up', 'down'], ['left', 'right']]
-three = [['up','right'],['right','down'],['down','left'],['left','up']]
-four = [['left','up','right'],['up','right','down'],['right','down','left'],['down','left','up']]
-five = [['up','down','left','right']]
+# up, down, left, right
+# 0     1    2      3
+row_dr = [0,0,-1,1]
+col_dr = [-1,1,0,0]
+cctv = [
+    [], 
+    [[0],[1],[2],[3]], # cctv1
+    [[0,1],[2,3]], # cctv2
+    [[0,3],[3,1],[1,2],[0,2]], # cctv3
+    [[0,1,2],[1,2,3],[2,3,0],[3,0,1]], # cctv4
+    [[0,1,2,3]] # cctv5
+]
+
+q = deque()
 for i in range(N):
     for j in range(M):
-        if arr[i][j]==1:
-            for k in one:
-                
+        if arr[i][j]!=0 and arr[i][j]!=6:
+            q.append([i,j])
+dfs()
