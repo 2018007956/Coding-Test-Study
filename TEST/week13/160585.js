c0b03dfd8e4bc5830f46d71336e17d794e7b5771
KyeongHa <rudgk030@hanyang.ac.kr>
2023-05-16 21:59:29 +0900

    week13_KH

--- TEST/week13/160585.js
+++ TEST/week13/160585.js
@@ -9,41 +9,47 @@ function findNumber(arr, el){
     return number;
 }
 
-//세로
+// 가로
+// [0][0]
 // [0][1]
 // [0][2]
-// [0][3]
 
+// [1][0]
 // [1][1]
 // [1][2]
-// [1][3]
 
 // ...
 
+
 function findHorizontalLine(arr){
+    let total = 0;
+    
     for(let i=0; i<arr.length; i++){
-    let number = 0;
+        let number = 0;
         for(let j=0; j<arr.length-1; j++){
-            if(arr[i][j] == arr[i][j+1] && arr[i][j] != '.'){
-                if(number >= 1) return false
-                else number++;
-            }
+            if(arr[i][j] == arr[i][j+1] && arr[i][j] != '.')
+                number++;
         }
+        if(number >= 2) total++;
+        if(total >= 2) return total;
      }
-    return true;
+    return total;
 }
 
 function findVerticalLine(arr){
-    for(let i=0; i<arr.length-1; i++){
+    let total = 0;
+    
+    for(let i=0; i<arr.length; i++){
     let number = 0;
-        for(let j=0; j<arr.length; j++){
-            if(arr[i][j] == arr[i+1][j] && arr[i][j] != '.'){
-                if(number >= 1) return false;
-                else number++;
-            }
+        for(let j=0; j<arr.length-1; j++){
+            if(arr[j][i] == arr[j+1][i] && arr[j][i] != '.'){
+                number++;
         }
-     }
-    return true;
+        if(number >= 2) total++;
+        if(total >= 2) return total;
+        }
+    }
+    return total;
 }
 
 //대각선
@@ -56,32 +62,36 @@ function findVerticalLine(arr){
 // [2][0]
 
 function findDiagonalLine(arr){
-    for(let i=0; i<arr.length-1; i++){
-        let number = 0;
-        for(let j=0; j<arr.length-1; j++){
-            if(arr[i][j] == arr[i+1][j+1] && arr[i][j] != '.'){
-                if(number >= 1) return false;
-                else number++;
-            }
-        }
-     }
-    return true;
+    let total = 0;
+    if(arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2]) 
+        if(arr[0][0] != '.') total++;
+    if(arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0])
+        if(arr[0][2] != '.') total++;
+    
+    return total;
 }
 
 function solution(board) {
     // board가 정상일 조건
-
     // 1) O의 개수 >= X의 개수
+    // 2) 가로, 세로, 대각선에 대해 1번 이하의 줄이 있어야 함
+    
+    const numberOfHorizontal = findHorizontalLine(board);
+    const numberOfVertical = findVerticalLine(board);
+    const numberOfDiagonal = findDiagonalLine(board);
+    console.log(numberOfDiagonal)
+    
     const numberOfO = findNumber(board, 'O');
     const numberOfX = findNumber(board, 'X');
-    if(numberOfO < numberOfX) return 0;
-    else if(numberOfO == 0 && numberOfX == 0) return 1;
-    else if(numberOfO+numberOfX == 9) return 1;
-
-    // 2) 가로, 세로, 대각선에 대해 1번 이하의 줄이 있어야 함
-    if (findHorizontalLine(board) == false) return 0;
-    else if(findVerticalLine(board) == false) return 0;
-    else if(findDiagonalLine(board) == false) return 0;
     
+    if(numberOfO > numberOfX + 2 || numberOfO < numberOfX) return 0;
+    // else if(numberOfO == 0 && numberOfX == 0) return 1;
+    // else if(numberOfO+numberOfX == 9) return 1;
+    
+    if(numberOfHorizontal + numberOfVertical + numberOfDiagonal >= 2) 
+        if(numberOfO+numberOfX == 9) return 1;
+        else if(numberOfHorizontal >= 1 && numberOfVertical >= 1 && numberOfDiagonal >= 1) 
+            return 1;
+        else return 0;
     return 1;
 }
