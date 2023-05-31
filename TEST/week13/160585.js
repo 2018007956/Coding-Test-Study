c629af1a1e9934280d538c1f9ad47c4167b9e27a
KyeongHa <rudgk030@hanyang.ac.kr>
2023-05-31 22:11:33 +0900

    week13_KH

--- TEST/week13/160585.js
+++ TEST/week13/160585.js
@@ -1,60 +1,70 @@
 let linesO = 0;
-let linesX = 0;
-// detect된 O와 X의 빙고 개수
+let linesX = 0; // detect된 O와 X의 빙고 개수
 
 function findNumber(arr, el){
     let number = 0;
     for(let i=0; i<arr.length; i++){
-        for(let j=0; j<arr[i].length; j++){
+        for(let j=0; j<arr[i].length; j++)
             if(arr[i][j] == el) number++;
-        }
     }
     return number;
 }
 
 function findHorizontalLine(arr){
     for(let i=0; i<arr.length; i++){
-        let number = 0;
-        for(let j=0; j<arr.length-1; j++){
-            if(arr[i][j] == arr[i][j+1] && arr[i][j] != '.') number++;
+        let matchX = 0;
+        let matchO = 0;
         
-            if(number >= 2){
-                if(arr[i][j] == 'O') linesO += 1;
-                else linesX += 1;
-            }
-        }
+         for(let j=0; j<arr.length; j++){
+             if(arr[i][j] == 'O') matchO++;
+             else if(arr[i][j] == 'X') matchX++;
+         }
+        
+        if(matchO >=3) linesO++;
+        else if(matchX >=3) linesX++;
     }
 }
 
 function findVerticalLine(arr){
     for(let i=0; i<arr.length; i++){
-    let number = 0;
-        for(let j=0; j<arr.length-1; j++){
-            if(arr[j][i] == arr[j+1][i] && arr[j][i] != '.') number++;
-            if(number >= 2){
-                if(arr[i][j] == 'O') linesO += 1;
-                else linesX += 1;
-            }
-        }
+        let matchX = 0;
+        let matchO = 0;
+        
+         for(let j=0; j<arr.length; j++){
+             if(arr[j][i] == 'O') matchO++;
+             else if(arr[j][i] == 'X') matchX++;
+         }
+        
+        if(matchO >=3) linesO++;
+        else if(matchX >=3) linesX++;
     }
 }
 
 function findDiagonalLine(arr){
-    if(arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2]) {
-        if(arr[0][0] == 'O') linesO += 1;
-        else if(arr[0][0] == 'X') linesX += 1;
+    let matchX = 0;
+    let matchO = 0;
+    
+    for(let i=0; i<arr.length; i++){
+        if(arr[i][i] == 'O') matchO++;
+        else if(arr[i][i] == 'X') matchX++;
     }
-    if(arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0]) {
-        if(arr[0][2] == 'O') linesO += 1;
-        else if(arr[0][2] == 'X') linesX += 1;
+    
+    if(matchO >= 3) linesO++;
+    else if(matchX >= 3) linesX++;
+    
+    matchX = 0;
+    matchO = 0;
+    
+    for(let i=0; i<arr.length; i++){
+        if(arr[i][2-i] == 'O') matchO++;
+        else if(arr[i][2-i] == 'X') matchX++;
     }
+    
+    if(matchO >= 3) linesO++;
+    else if(matchX >= 3) linesX++;
 }
 
 function solution(board) {
-    // board가 정상일 조건
-    // 1) O의 개수 >= X의 개수
-    // 2) 가로, 세로, 대각선에 대해 1번 이하의 줄이 있어야 함
-    
     findHorizontalLine(board);
     findVerticalLine(board);
     findDiagonalLine(board);
@@ -63,10 +73,7 @@ function solution(board) {
     const numberOfO = findNumber(board, 'O');
     const numberOfX = findNumber(board, 'X');
     
-    console.log(linesO)
-    console.log(linesX)
-    
-    if(numberOfO + numberOfX == 0) return 1;
+    if(numberOfO + numberOfX == 0) return 1; // 보드판이 모두 . 인 경우
     if(numberOfO < numberOfX) return 0; // X의 차례가 아닌 경우
     if(numberOfO > numberOfX + 1) return 0; // O의 차례가 아닌 경우
     if(numberOfO == numberOfX && linesO >= 1) return 0; // O의 차례에서 이미 빙고가 나온 경우(테케 2번)
